d81d282105ae4afd57942dda7c4b0f4ee06623e9
Renato Henrique <renato.henrique_98@hotmail.com>
2019-11-30 04:22:01 -0300

    Removendo documentação do projeto base

--- src/main/java/com/github/throyer/brinquedoteca/app/configuration/security/SrpingSecurityConfiguration.java
+++ src/main/java/com/github/throyer/brinquedoteca/app/configuration/security/SrpingSecurityConfiguration.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2019 Renato Henrique
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package com.github.throyer.brinquedoteca.app.configuration.security;
 
 import static com.github.throyer.brinquedoteca.app.utils.SecurityConstants.*;
@@ -27,13 +11,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-/**
- * Security Configuration.
- * 
- * Classe de configurações de segurança. 
- * @author Renato Henrique.
- * @since 3.0.0.
- */
 @Configuration
 public class SrpingSecurityConfiguration extends WebSecurityConfigurerAdapter {
     
